65f728ace8193329449211a053c980ca8b76b33e
Haley Moses <vhr825@gmail.com>
2022-05-29 11:21:37 -0500

    added smooth scrolling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./navbar.scss";
-
+import { Link } from "react-scroll";
+//smooth scrolling
 export default function Navbar() {
   return (
 
@@ -8,29 +9,57 @@ export default function Navbar() {
 
         <nav className="navcontainer" id="navbarcontainer">
           <ul className="nav-list">
-
           <li className="aboutnav" id="aboutnavbar">
+          <Link to="about"
+              smooth={true}
+              offset={-70}
+              duration={500}
+              spy={true}
+              >
             <a href='#about' className="aboutbtn" id="aboutbtnnav">
               About Me
             </a>
+            </Link>
           </li>
+      
 
             <li className="projectsnav" id="projectsnavbar">
+            <Link to="projects"
+              smooth={true}
+              offset={-70}
+              duration={500}
+              spy={true}
+              >
             <a href="#projects" className="projectsbtn" id="projectsbtnnav" >
               Projects
             </a>
+            </Link>
             </li>
 
             <li className="skillsnav" id="skillsnavbar">
+            <Link to="skills"
+              smooth={true}
+              offset={-70}
+              duration={500}
+              spy={true}
+              >
             <a href="#skills" className="skillsbtn" id="skillsbtnnav">
               Skills
             </a>
+            </Link>
             </li>
 
           <li className="contactnav" id="contactnavbar">
+          <Link to="contact"
+              smooth={true}
+              offset={-70}
+              duration={500}
+              spy={true}
+              >
             <a href="#contact" className="contactbtn" id="contactbtnnav">
               Contact Me
             </a>
+            </Link>
           </li>
 
           </ul>

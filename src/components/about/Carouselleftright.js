e67d41116a76853c5a226628065241336ecf1be8
Haley Moses <vhr825@gmail.com>
2022-07-10 15:11:34 -0500

    redid the carousel and made it responsive and show 2 elements at a time

--- src/components/about/Carouselleftright.js
+++ src/components/about/Carouselleftright.js
@@ -1,48 +1,84 @@
 /* eslint-disable no-eval */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { imagesArray } from '../../data';
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import Carousel from "./Carousel.js";
 
 
 
   
 const Carouselleftright = () => {
  
-    
-  
+    const [width, setWidth]   = useState(window.innerWidth);
+    const updateDimensions = () => {
+        setWidth(window.innerWidth);
+    }
+    useEffect(() => {
+        window.addEventListener("resize", updateDimensions);
+        return () => window.removeEventListener("resize", updateDimensions);
+    }, []);
 
     return (
-     
-         <div className='imageoutercontainer' >
-            <Carousel>
-             {imagesArray.map((item) => (
-                <div className='image' id={item.id} style={{
-                    'display': 'block',
-                    }}
-                    >
-                    <img
-                        src={item.img}
-                        alt={item.alt}
-                        width={`300px`}
-                        className={item.className}
-                    />
-                    <div className="image__overlay"
-                    style={{
-                        'display': 'flex',
-                        'flexFlow': 'wrap',
-                    }}>
-                        <p style={{
-                        'display': 'flex',
-                        'flexFlow': 'wrap',
-                    }}>{item.message}</p>
-                    </div>
-                </div>   
-             ))}   
-             </Carousel> 
- 
-             </div>
- 
+        (width<800) ? 
+        (<div className='imageoutercontainer' style={{ maxWidth: 388, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}>
+        <Carousel
+            show={1}
+            infiniteLoop={true}>
+         {imagesArray.map((item) => (
+            <div className='image' id={item.id} style={{
+                'display': 'block',
+                }}
+                >
+                <img
+                    src={item.img}
+                    alt={item.alt}
+                    className={item.className}
+                />
+                <div className="image__overlay"
+                style={{
+                    'display': 'flex',
+                    'flexFlow': 'wrap',
+                }}>
+                    <p style={{
+                    'display': 'flex',
+                    'flexFlow': 'wrap',
+                }}>{item.message}</p>
+                </div>
+            </div>   
+         ))}   
+         </Carousel> 
+
+         </div>
+) : (<div className='imageoutercontainer' style={{ maxWidth: 776, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}>
+<Carousel
+    show={2}
+    infiniteLoop={true}>
+ {imagesArray.map((item) => (
+    <div className='image' id={item.id} style={{
+        'display': 'block',
+        }}
+        >
+        <img
+            src={item.img}
+            alt={item.alt}
+            className={item.className}
+        />
+        <div className="image__overlay"
+        style={{
+            'display': 'flex',
+            'flexFlow': 'wrap',
+        }}>
+            <p style={{
+            'display': 'flex',
+            'flexFlow': 'wrap',
+        }}>{item.message}</p>
+        </div>
+    </div>   
+ ))}   
+ </Carousel> 
+
+ </div>
+)
+         
 
     );
 }

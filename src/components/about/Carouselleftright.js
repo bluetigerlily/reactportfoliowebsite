cbb66aac7cf974970b661bcb3e59520b1d3544c9
Haley Moses <vhr825@gmail.com>
2022-07-19 19:40:18 -0500

    fixing mobile

--- src/components/about/Carouselleftright.js
+++ src/components/about/Carouselleftright.js
@@ -8,59 +8,18 @@ import Carousel from "./Carousel.js";
 const Carouselleftright = () => {
  
     const [width, setWidth]   = useState(window.innerWidth);
-    const [ifMobile, setIfMobile] = useState(false);
 
     const updateDimensions = () => {
         setWidth(window.innerWidth);
     }
-    const updateIsMobile = (width) => {
-        if (width <= 388) {
-        setIfMobile(true)
-        }
-    }   
+ 
     useEffect(() => {
         window.addEventListener("resize", updateDimensions);
         return () => window.removeEventListener("resize", updateDimensions);
     }, []);
-    useEffect(() => {
-        window.addEventListener("resize", updateIsMobile);
-        return () => window.removeEventListener("resize", updateIsMobile); 
 
-    }, []);
 
     return (
-        (ifMobile) ? (<div className='imageoutercontainer' style={{ maxWidth: 300, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}>
-        <Carousel
-            show={1}
-            infiniteLoop={true}>
-         {imagesArray.map((item) => (
-            <div className='image' id={item.id} style={{
-                'display': 'block',
-                'width': '250',
-                }}
-                >
-                <img
-                    src={item.img}
-                    alt={item.alt}
-                    className={item.className}
-                />
-                <div className="image__overlay"
-                style={{
-                    'display': 'flex',
-                    'flexFlow': 'wrap',
-                    'width': '250',
-                }}>
-                    <p style={{
-                    'display': 'flex',
-                    'flexFlow': 'wrap',
-                }}>{item.message}</p>
-                </div>
-            </div>   
-         ))}   
-         </Carousel> 
-        
-         </div>
-        ) : (
         (width<800) ?
         (<div className='imageoutercontainer' style={{ maxWidth: 388, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}>
 <Carousel
@@ -121,7 +80,7 @@ const Carouselleftright = () => {
          </Carousel> 
 
          </div>
-))
+)
         
     );
 }

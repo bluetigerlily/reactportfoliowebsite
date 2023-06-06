9cfd5a0c883db357e93c89093a06c4ec1c20f291
Haley Moses <vhr825@gmail.com>
2023-06-06 00:43:32 -0500

    made a button component and added 5 images and made the toggle work

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import reactjs from '../../assests/reactjs.jpg';
 import './button.scss';
 
 const Button = () => {
+    
+    const [toggle, setToggle] = useState(false);
+    
     return (
         <div className="backgroundContainer">
             <div className="buttonContainer">
-                <button className="neumorphicBtn">-</button>
-                <img
-                src={reactjs}
-                alt="react js logo used for background parallax effect"
-                className="backgroundimgReact"/>
+                <button className="neumorphicBtn"
+                         onClick={() => setToggle(!toggle)}
+                         >-</button>
+                         {toggle && (
+                <div className="imgContainer">
+                    <img
+                    src={reactjs}
+                    alt="react js logo used for background parallax effect"
+                    className="backgroundimgReact__one"/>
+                    <img
+                    src={reactjs}
+                    alt="react js logo used for background parallax effect"
+                    className="backgroundimgReact__two"/>
+                    <img
+                    src={reactjs}
+                    alt="react js logo used for background parallax effect"
+                    className="backgroundimgReact__three"/>
+                    <img
+                    src={reactjs}
+                    alt="react js logo used for background parallax effect"
+                    className="backgroundimgReact__four"/>
+                    <img
+                    src={reactjs}
+                    alt="react js logo used for background parallax effect"
+                    className="backgroundimgReact__five"/>
+                </div>
+             )}
             </div>
        </div>
     );
